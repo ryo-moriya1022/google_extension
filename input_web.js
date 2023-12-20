6e5e692d0e3b66a870b6a18193cbdf2ad2d40695
1 <[regu7828@gmail.com]>
2023-12-20 15:17:35 +0900

    2023/12/19の変更

---
+++ input_web.js
@@ -0,0 +1,16 @@
+
+const s_buttons = document.querySelectorAll("button");
+s_buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+        // ボタンがクリックされたときに関連するinputの値を取得
+        const relatedInput = button.closest("form").querySelector("input[type=password]");
+        const inputValue = relatedInput.value;
+        // ボタンがクリックされたときにresizeTargetのテキストとinputの値を表示
+        const resizeTarget = document.getElementById('resizeTarget').querySelector('.mainttl2 span');
+        console.log("resizeTarget:", resizeTarget.textContent);
+        console.log("inputValue:", inputValue);
+        
+        test();
+    });
+});
+
