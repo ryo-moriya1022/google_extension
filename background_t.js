9729d9d1e9a880212de1522182316887b056da07
1 <[regu7828@gmail.com]>
2024-01-29 13:18:21 +0900

    2024/01/29

--- background_t.js
+++ background_t.js
@@ -1,26 +1,28 @@
 function output(){
-    var url = "https://script.google.com/macros/s/AKfycbzRks9HFUK7eJ_pCgd8-FHocaLcuyNLIW1hgcWHHuwVn5mrtl73Rqo8I3TPJDT0k9YY8g/exec"
+    var url = "https://script.google.com/macros/s/AKfycbyQ27jkvr_Qqfkj4VovK__ZRNWva8rKzbDXfM_9rhxgATlbZHko8gSwyTbYBI34dMY9_g/exec"
     fetch(url)
-        .then(function (res) {
-            return res.json(); // フェッチしたデータを JSON 形式に変換
-        })
-        .then(function (jsonData) {
-                console.log(jsonData); // JSON へ変換されたデータをコンソールに表示
-            })
-    .catch(error=>{
-        console.error('通信に失敗しました', error);
-    });}
+    .then(response => response.json())
+    .then(data => {
+      console.log("取得したデータ:", data);
+      // ここで取得したデータを処理する
+    })
+    .catch(error => {
+      console.error("データの取得に失敗しました:", error);
+    });
+}
 
 function input(classname, psw){
-    var url="https://script.google.com/macros/s/AKfycbyevKgZORmzvkQHELCknBNfXRxNY2lq8CAf035RSxIstK7kDOcv9-Tc64ejsGpzJ7NWfw/exec";
+    var url="https://script.google.com/macros/s/AKfycbwr6mjpxo8DddJHlFKuripwGSOoa3qa5deW7jMYvVFKb7dSkLsbXCG21S7hYS5dWsrVcw/exec";
     fetch(url+"?key1="+classname+"&key2="+psw,
         {
         //method: 'HEAD',
-        mode: 'no-cors'
-        })
+        mode: 'cors',
+        redirect: 'follow' 
+        },
+        )
         .then(response=>{
-            console.log(response);
-            return response.text();
+            console.log("fetchが実行されました");
+            console.log("Response Status: " + response.status);
         })
         }
 
@@ -28,8 +30,6 @@ chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
     psw=message.text1;
     classname=message.text2;
-    response=input(classname,psw);
-    sendResponse({res:response},function(response){
-        console.log(response);
-    });
+    input(classname,psw);
+    sendResponse({res:"success"});
 });
